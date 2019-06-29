bb1b1597744358f507f9463496690a159e70aa0e
PowerOfEvil <52323616+PowerOfEvil614@users.noreply.github.com>
2019-06-29 16:07:27 +0800

    Revised Player.h

--- src/Player.h
+++ src/Player.h
@@ -10,11 +10,15 @@ public:
 	Player(int x,int y);
 
 	b2ParticleSystem* particleSystem;
+	b2ParticleGroup* playerGroup;
 
 	friend b2ParticleSystem;
+	friend b2ParticleGroup;
 
+	b2Vec2 getLinearVelocity() const{ return playerGroup->GetLinearVelocity(); }
+	void applyLinearImpluse(const b2Vec2& impluse) { playerGroup->ApplyLinearImpulse(impluse); }
+	const b2Vec2 &getPosition() const { return playerGroup->GetPosition(); }
 private:
 	b2CircleShape playerShape;
 	b2ParticleGroupDef playerDef;
-
 };
