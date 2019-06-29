9ef8fed531560c4ecca9659c61598fb18ddde359
libricardo <libricardo@users.noreply.github.com>
2019-06-29 15:44:39 +0800

    update Bubble & create Ground
    also change a little in GameObject by adding a getWorld() function

--- src/Bubble.h
+++ src/Bubble.h
@@ -3,5 +3,11 @@
 
 class Bubble :public Trigger {
 public:
-	Bubble();
+	Bubble(Vector2& pos, int size, int impact);
+	void putIntoWorld();
+	~Bubble() {}
+private:
+	Vector2 bubblePos;
+	int bubbleSize;
+	int bubbleImpact;//color has something to do with its impact
 };
--- src/GameObject.h
+++ src/GameObject.h
@@ -1,9 +1,13 @@
 #pragma once
+#include "raylib.h"
+#include "Box2D/Box2D.h"
 
 class GameObject {
 public:
 	virtual ~GameObject() = 0;
-
+	b2World* getWorld() { return m_world; }
 
 	GameObject();
+private:
+	b2World* m_world;
 };
